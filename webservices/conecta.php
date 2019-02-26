3c5ec33032c691227eb54a6497fcf3131e18a979
José Roberto Lira <joserobertolira@gmail.com>
2019-02-26 19:04:30 -0300

    Update Conecta

--- webservices/conecta.php
+++ webservices/conecta.php
@@ -1,8 +1,6 @@
 <?php 
-// $conexao = mysqli_connect('appnagumo.mysql.dbaas.com.br', 'appnagumo','Magic@2015');
 date_default_timezone_set('America/Sao_Paulo');
 $conexao = mysqli_connect('', '','', '');
-//mysqli_select_db($conexao, 'appnagumo');
 mysqli_set_charset($conexao,"utf8");
 
 
